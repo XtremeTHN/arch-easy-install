c1307d4ef53c9743ecf6f584394c172b6e9f3039
XtremeTHN <nigthmaresans2@gmail.com>
2022-11-25 12:24:03 -0600

    1.4

--- misc/pm_aur.py
+++ misc/pm_aur.py
@@ -2,9 +2,9 @@ from os import system as execute
 from sys import argv
 
 if argv[1] == "paru":
-    execute("su axel ./inst_paru.sh")
+    execute(f"su {argv[1]} ./inst_paru.sh")
     sys.exit(0)
 elif argv[1] == "yay":
-    execute("su axel ./inst_yay.sh")
+    execute(f"su {argv[1]} ./inst_yay.sh")
     sys.exit(0)
 sys.exit(1)
