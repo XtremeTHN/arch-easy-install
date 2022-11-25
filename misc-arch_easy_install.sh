dbd5e2c4275e75df0c4bfa2fb25a52e4543205b3
XtremeTHN <nigthmaresans2@gmail.com>
2022-11-24 19:53:04 -0600

    0.1

--- misc-arch_easy_install.sh
+++ misc-arch_easy_install.sh
@@ -11,43 +11,45 @@ DIR=/sys/firmware/efi/
 RED="\x1b[31m"
 RESET="\x1b[0m"
 echo "Bienvenido al script de instalacion BSPWM, comenzará la instalacion automaticamente"
-
-if [ -e "/tmp/ssid_tmp" ]; then
-	echo "Utilizar internet introducido anteriormente?"
-	if [ $(gum choose "Si" "No") = "Si" ]; then
-		iwctl station wlan0 connect $SSID password $WIFI_PASS
-	fi
-fi
-echo "Introduce el nombre del internet (SSID)"
-SSID=$(gum input --placeholder "Nombre del internet")
-echo "Introduce la contraseña de tu internet"
-if [ "$SSID" = "" ]; then
-  echo "Nombre de internet no especificado, saliendo"
-  exit
-fi
-WIFI_PASS=$(gum input --placeholder "Contraseña")
-if [ "$WIFI_PASS" = "" ]; then
-  echo "Contraseña no especificada, saliendo"
-  exit
-fi
-echo "Se conectara a internet mediante iwctl (dhcpcd)"
-echo $SSID > /tmp/ssid_tmp
-echo $WIFI_PASS > /tmp/wifi_tmp
-
-iwctl station wlan0 connect $SSID password $WIFI_PASS
-
+ping -c 1 google.com > /dev/null
 if [ "$?" -gt 0 ]; then
-	echo "$RED [ERR] $RESET Ha habido un problema al conectarse al internet"
-	exit
+
+  if [ -e "/tmp/ssid_tmp" ]; then
+	  echo "Utilizar internet introducido anteriormente?"
+	  if [ $(gum choose "Si" "No") = "Si" ]; then
+		  iwctl station wlan0 connect $SSID password $WIFI_PASS
+	  fi
+  fi
+  echo "Introduce el nombre del internet (SSID)"
+  SSID=$(gum input --placeholder "Nombre del internet")
+  echo "Introduce la contraseña de tu internet"
+  if [ "$SSID" = "" ]; then
+    echo "Nombre de internet no especificado, saliendo"
+    exit
+  fi
+  WIFI_PASS=$(gum input --placeholder "Contraseña")
+  if [ "$WIFI_PASS" = "" ]; then
+    echo "Contraseña no especificada, saliendo"
+    exit
+  fi
+  echo "Se conectara a internet mediante iwctl (dhcpcd)"
+  echo $SSID > /tmp/ssid_tmp
+  echo $WIFI_PASS > /tmp/wifi_tmp
+
+  iwctl station wlan0 connect $SSID password $WIFI_PASS
+
+  if [ "$?" -gt 0 ]; then
+	  echo "$RED [ERR] $RESET Ha habido un problema al conectarse al internet"
+	  exit
+  fi
+
+  ping -c 1 www.google.com > /dev/null
+
+  if [ "$?" -eq 2 ]; then
+    echo "$RED [ERR] $RESET Has introducido una contraseña incorrecta, SSID incorrecto o no hay internet, sin internet no se podran descagar los paquetes, saliendo... "
+    exit
+  fi
 fi
-
-ping -c 1 www.google.com > /dev/null
-
-if [ "$?" -eq 2 ]; then
-  echo "$RED [ERR] $RESET Has introducido una contraseña incorrecta, SSID incorrecto o no hay internet, sin internet no se podran descagar los paquetes, saliendo... "
-  exit
-fi
-
 gum confirm "Se formateara el disco que selecciones y los archivos seran eliminados, ¿Deseas continuar?"
 
 echo "Las particiones tendrás que crearlas manualmente, predeterminadamente solo se ocupan 3 particiones, una particion para la raiz del sistema, una para el booteo y otra para archivos de intercambio (swap)"
